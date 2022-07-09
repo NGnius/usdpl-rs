87c5a7770b234eb45a6bc3ab12f4c8b5f938af66
NGnius (Graham) <ngniusness@gmail.com>
2022-07-09 12:28:33 -0400

    Fix read_single parsing failure due to \n

--- usdpl-back/src/api_common/files.rs
+++ usdpl-back/src/api_common/files.rs
@@ -15,7 +15,7 @@ pub fn write_single<P: AsRef<Path>, D: Display>(path: P, display: D) -> Result<(
 /// Read something from a file.
 /// Useful for kernel configuration files.
 pub fn read_single<P: AsRef<Path>, D: FromStr<Err=E>, E>(path: P) -> Result<D, (Option<io::Error>, Option<E>)> {
-    let mut file = File::create(path).map_err(|e| (Some(e), None))?;
+    let mut file = File::open(path).map_err(|e| (Some(e), None))?;
     let mut string = String::new();
     file.read_to_string(&mut string).map_err(|e| (Some(e), None))?;
     string.trim().parse().map_err(|e| (None, Some(e)))
